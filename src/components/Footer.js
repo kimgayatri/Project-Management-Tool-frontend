f958bee6ea17ff5f5e1802051612a8418a5b7a48
Gayatri <gayatrinehe6@gmail.com>
2025-02-22 19:50:33 +0530

    Project Management Tool with React and ExpressJS Framework

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+ /* eslint-disable jsx-a11y/anchor-is-valid */
+export default function Footer() {
+    return (
+        <footer className="foo">
+            <p>Created by <a href="https://github.com/kimgayatri" target="_blank" rel="noopener noreferrer"> Gayatri Nehe⭐</a></p>
+        </footer>
+    )
+}
+ 
